37c7f0dbeaf5e2ab109a2c172bdcf7a9887a5f51
瓜子 <89156163+julyguazi@users.noreply.github.com>
2022-10-16 15:55:18 +0800

    Update dyjsb.js

--- ycty/dyjsb.js
+++ ycty/dyjsb.js
@@ -6,7 +6,6 @@ while(i!=10){
    var tx1 = widget.desc()
 //获取其中心位置并点击
 click(widget.bounds().centerX(), widget.bounds().centerY());
-//如果用root权限则
 console.log("1.2")
 sleep(2000);
 var b = id("i+n").findOne();
@@ -27,49 +26,24 @@ if (wf == -1){
     var fs = 10000
 }
 console.log(fs)
-if (gz==1 && fs <500){
+    var ygz = id("hkp").findOne(1000);
+if (gz==1 && fs <500 && ygz == null){
+        if (id("title").exists()){
+        toast("私密账号，不关注")
+        var widget = id("back_btn").findOne();
+        click(widget.bounds().centerX(), widget.bounds().centerY());
+    }else{
     toast("账号符合，点击关注")
-    var dyc = id("i+q").findOne();
-    sleep(1000)
-    var i=i+1
     var widget = id("hko").findOne();
     click(widget.bounds().centerX(), widget.bounds().centerY());
     sleep(1000)
     var widget = id("back_btn").findOne();
     click(widget.bounds().centerX(), widget.bounds().centerY());
-    sleep(1000)
-    var widget = id("avatar").findOne();
-    click(widget.bounds().centerX(), widget.bounds().centerY());
-    sleep(3000)
-    var dec = id("i+q").findOne();
-    if ( dyc.text() == dec.text() ){
-        var widget = id("hko").findOne();
-        click(widget.bounds().centerX(), widget.bounds().centerY());
-        //重新关注
-        sleep(1000)
-        //等待一秒
-        var widget = id("idz").findOne();
-        click(widget.bounds().centerX(), widget.bounds().centerY());
-        //点击私信
-        sleep(1000)
-        var widget = id("ge3").findOne();
-        click(widget.bounds().centerX(), widget.bounds().centerY());
-        setText("你好啊,最近谷票收益怎么样？")
-        sleep(1000)
-        var widget = id("adw").findOne();
-        click(widget.bounds().centerX(), widget.bounds().centerY());
-        //发送
-        sleep(1000)
-        var widget = id("fkl").findOne();
-        click(widget.bounds().centerX(), widget.bounds().centerY());
-        sleep(1000)
-        var widget = id("back_btn").findOne();
-        click(widget.bounds().centerX(), widget.bounds().centerY());
-        //退出
     }else{
         var widget = id("back_btn").findOne();
         click(widget.bounds().centerX(), widget.bounds().centerY());
     }
+}
 }else{
     toast("没用账号，退出")
     var widget = id("back_btn").findOne();
