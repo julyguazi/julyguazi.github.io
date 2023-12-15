d487de3898847bada67d1a0a4f75f5ab60fad263
瓜子 <89156163+julyguazi@users.noreply.github.com>
2023-12-16 00:41:57 +0800

    Update yylb.js

--- api/test/yylb.js
+++ api/test/yylb.js
@@ -1,1 +1,1 @@
-{"status":1,"name":"莫甘娜1","img":"https://ossweb-img.qq.com/images/lol/web201310/skin/big81005.jpg","username":"腾讯","down":"https://txlou.club/","data":[],"ts":1702644692771}
+{"status":1,"name":"莫甘娜1","img":"https://ossweb-img.qq.com/images/lol/img/champion/Morgana.png","username":"腾讯","down":"https://txlou.club/","data":[],"ts":1702644692771}
